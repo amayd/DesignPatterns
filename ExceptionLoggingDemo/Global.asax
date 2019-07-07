97c0a9664dd860a9156198524e07993ee42515a8
Ameya Deshpande <P10473049@capita.co.uk>
2019-07-07 16:54:47 +0530

    add logger using singletone design pattern

---
+++ ExceptionLoggingDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExceptionLoggingDemo.MvcApplication" Language="C#" %>
---
+++ Logger/Log.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public sealed class Log : ILog
+    {
+
+        // making variable accessable only in class.
+        private static Log instance = null;
+
+        // provide public static property to access reference to instance of the class.
+        public static Log GetInstance
+        {
+            get
+            {
+                // if instance is null then only create a instance otherwise return existing object.
+                if (instance == null)
+                {
+                    //creating instance
+                    instance = new Log();
+                }
+                // returning instance
+                return instance;
+            }
+        }
+
+        // making private contructors so instance will not be created outside of the class.
+        private Log()
+        {
+
+        }
+
+        public void LogException(string message)
+        {
+            string filename = $"Exception_{DateTime.Now.ToShortDateString()}";
+            string filePath = $"{AppDomain.CurrentDomain.BaseDirectory}\\{filename}";
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("---------------------------------------");
+            stringBuilder.Append($" Date : {DateTime.Now.ToString()}");
+            stringBuilder.Append(message);
+
+            
+            using(StreamWriter streamWriter = new StreamWriter(filePath, true))
+            {
+                streamWriter.Write(stringBuilder.ToString());
+                streamWriter.Flush();
+            }
+        }
+    }
+}
